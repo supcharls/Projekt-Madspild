61ffa251b07d5ab9cf73beb67799583b9496e779
supcharls <charlie_xian@hotmail.com>
2025-04-10 23:30:31 +0200

    kommentar tilføjelse

--- src/main/java/org/example/projektmadspild/Controller/FaktaController.java
+++ src/main/java/org/example/projektmadspild/Controller/FaktaController.java
@@ -3,13 +3,13 @@ package org.example.projektmadspild.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
-@Controller
+@Controller // Fortæller Spring boot at denne klasse skal håndtere web anmodninger
 public class FaktaController
 {
-    @GetMapping("/fakta")
+    @GetMapping("/fakta") // Fanger "get-requests" til stien "/fakta"
     public String visFakta()
     {
-        return "fakta";
+        return "fakta"; // returnerer navnet på html filen - Spring leder altså efter "fakta.html" i vores template mappe
     }
 
 }
