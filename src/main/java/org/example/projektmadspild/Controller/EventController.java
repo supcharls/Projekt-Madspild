f2652a57b23cd54ec458171281fbadd2bc106be2
supcharls <charlie_xian@hotmail.com>
2025-04-10 22:25:59 +0200

    css update tjek

--- src/main/java/org/example/projektmadspild/Controller/EventController.java
+++ src/main/java/org/example/projektmadspild/Controller/EventController.java
@@ -4,7 +4,6 @@ import org.example.projektmadspild.Model.Event;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
